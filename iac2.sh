5cc76314c277c5aa72b7cb2cbdbd995e09eb219e
dennerferreira <111301158+dennerferreira@users.noreply.github.com>
2022-10-03 13:34:18 -0300

    Adicionando arquivo da atividade

---
+++ iac2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+echo " --- Atualizando o servidor ---"
+echo ""
+
+apt-get update
+apt-get upgrade -y
+
+echo "Baixando e copiando os arquivos da aplicação..."
+
+apt-get install apache2 -y
+apt-get install unzip -y
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html
+
